68e90edd9c860830be86dde746bfb62e84475112
hp <bindureddy4755@gmail.com>
2021-05-05 16:41:54 +0530

    initial commit

---
+++ springboot-crudrest-1/src/main/java/com/example/demo/SpringbootCrudrest1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringbootCrudrest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudrest1Application.class, args);
+	}
+
+}
